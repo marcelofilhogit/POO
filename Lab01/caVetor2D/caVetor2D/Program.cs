c823b0e722b1c240690eb41915a7ca769c528991
Marcelo Gonçalves dos Reis Filho <marcelofilhoufu@gmail.com>
2018-11-06 14:56:19 -0200

    Laboratório 01 - Vetor2D

---
+++ Lab01/caVetor2D/caVetor2D/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ca_lab01_vetor2d
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vetor2D objVet1 = new Vetor2D();
+            Vetor2D objVet2 = new Vetor2D();
+            Vetor2D objProdEsc = new Vetor2D();
+            Vetor2D objAngulo = new Vetor2D();
+            Vetor2D objProjecao = new Vetor2D();
+
+            double x;
+            double y;
+
+            Console.WriteLine("Digite o valor de X do vetor 1: ");
+            x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nDigite o valor de Y do vetor 1: ");
+            y = double.Parse(Console.ReadLine());
+
+            objVet1.setX(x);
+            objVet1.setY(y);
+
+            Console.WriteLine("\nDigite o valor de X do vetor 2: ");
+            x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nDigite o valor de Y do vetor 2: ");
+            y = double.Parse(Console.ReadLine());
+
+            objVet2.setX(x);
+            objVet2.setY(y);
+
+            Console.WriteLine("\nVetor 1 = ( " + objVet1.getX() + ", " + objVet1.getY() + " )" + "\nVetor 2 = ( " + objVet2.getX() + ", " + objVet2.getY() + " )");
+
+
+            objProdEsc.setProdutoEscalar(objVet1.getX(), objVet1.getY(), objVet2.getX(), objVet2.getY());
+            Console.WriteLine("\nProduto escalar do vetor 1 com o vetor 2 eh: " + objProdEsc.getProdutoEscalar());
+
+
+            objVet1.setModuloDoVetor(objVet1.getX(), objVet1.getY());
+            Console.WriteLine("\nO módulo do vetor 1 eh: " + objVet1.getModuloDoVetor());
+
+            objVet2.setModuloDoVetor(objVet2.getX(), objVet2.getY());
+            Console.WriteLine("O módulo do vetor 2 eh: " + objVet2.getModuloDoVetor());
+
+            objAngulo.setAnguloDosVetores(objProdEsc.getProdutoEscalar(), objVet1.getModuloDoVetor(), objVet2.getModuloDoVetor());
+            Console.WriteLine("\nO angulo entre os vetores 1 e 2 eh: " + objAngulo.getAnguloDosVetores());
+
+            objProjecao.setProjecao(objVet1.getX(), objVet1.getY(), objVet2.getX(), objVet2.getY());
+            Console.WriteLine("\nO Vetor projeção do vetores 1 em 2 eh: " + "( " + objProjecao.getProjecaoX() + ", " + objProjecao.getProjecaoY() + " )");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab01/caVetor2D/caVetor2D/Vetor2D.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ca_lab01_vetor2d
+{
+    class Vetor2D
+    {
+        //Atributos 
+        private double x;
+        private double y;
+        private double produtoEscalar;
+        private double moduloDoVetor;
+        private double anguloDosVetores;
+        private double projecaoX;
+        private double projecaoY;
+
+        //Metodo Construtor 
+        public Vetor2D()
+        {
+            x = 0;
+            y = 0;
+        }
+
+        //Metodo Construtor 
+        public Vetor2D(double _x, double _y)
+        {
+            x = _x;
+            y = _y;
+        }
+
+        //Metodos Get
+        public double getX()
+        {
+            return x;
+        }
+
+        public double getY()
+        {
+            return y;
+        }
+
+        //Metodos Set
+        public void setX(double _x)
+        {
+            x = _x;
+        }
+
+        public void setY(double _y)
+        {
+            y = _y;
+        }
+
+        //Produto escalar entre dois vetores
+        public double getProdutoEscalar()
+        {
+            return produtoEscalar;
+        }
+
+        public void setProdutoEscalar(double vet1X, double vet1Y, double vet2X, double vet2Y)
+        {
+            produtoEscalar = ((vet1X * vet2X) + (vet1Y * vet2Y));
+        }
+
+        //Módulo do vetor
+        public double getModuloDoVetor()
+        {
+            return moduloDoVetor;
+        }
+
+        public void setModuloDoVetor(double _x, double _y)
+        {
+            moduloDoVetor = Math.Sqrt((_x * _x) + (_y * _y));
+        }
+
+
+        //Angulo entre dois vetores
+        public double getAnguloDosVetores()
+        {
+            return anguloDosVetores;
+        }
+
+        public void setAnguloDosVetores(double _produtoEscalar, double _modV1, double _modv2)
+        {
+            anguloDosVetores = Math.Cos(_produtoEscalar / ((_modV1) * (_modv2)));
+        }
+
+
+        //Vetor projecão de um vetor a em um vetor b
+        public double getProjecaoX()
+        {
+            return projecaoX;
+        }
+
+        public double getProjecaoY()
+        {
+            return projecaoY;
+        }
+
+        public void setProjecao(double vet1X, double vet1Y, double vet2X, double vet2Y)
+        {
+            projecaoX = (((vet1X * vet2X) + (vet1Y * vet2Y)) * vet2X) / (((vet2X * vet2X) + (vet2Y * vet2Y)));
+            projecaoY = (((vet1X * vet2X) + (vet1Y * vet2Y)) * vet2Y) / (((vet2X * vet2X) + (vet2Y * vet2Y)));
+        }
+
+    }
+}
