d37ce623a0c168089292e78edb7d4a8b22398662
Marcelo Gonçalves dos Reis Filho <marcelofilhoufu@gmail.com>
2018-11-08 21:09:30 -0200

    Laboratório 06 - Arquivo Texto

---
+++ Lab06/wfaArquivoTexto/wfaArquivoTexto/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace wfaArquivoTexto
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbEntrada = new System.Windows.Forms.TextBox();
+            this.tbSaida = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Entre arquivo ou diretório:";
+            // 
+            // tbEntrada
+            // 
+            this.tbEntrada.Location = new System.Drawing.Point(33, 57);
+            this.tbEntrada.Name = "tbEntrada";
+            this.tbEntrada.Size = new System.Drawing.Size(265, 20);
+            this.tbEntrada.TabIndex = 1;
+            this.tbEntrada.KeyDown += new System.Windows.Forms.KeyEventHandler(this.tbEntrada_KeyDown);
+            // 
+            // tbSaida
+            // 
+            this.tbSaida.Location = new System.Drawing.Point(33, 92);
+            this.tbSaida.Multiline = true;
+            this.tbSaida.Name = "tbSaida";
+            this.tbSaida.Size = new System.Drawing.Size(265, 292);
+            this.tbSaida.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 396);
+            this.Controls.Add(this.tbSaida);
+            this.Controls.Add(this.tbEntrada);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Busca de Arquivo";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbEntrada;
+        private System.Windows.Forms.TextBox tbSaida;
+    }
+}
+
